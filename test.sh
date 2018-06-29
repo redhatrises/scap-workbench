35552406f187046024e379e16ecc8aae4d520cdf
Gabe <redhatrises@gmail.com>
2018-06-29 16:26:22 -0600

    TEST

--- test.sh
+++ test.sh
@@ -1,1 +1,3 @@
-wget `curl -s https://api.github.com/repos/openscap/scap-security-guide/releases/latest | jq -r \".assets[] | select(.name | test(\\\"(scap-security-guide-[0-9].[0-9].[0-9]*).zip\\\")) | .browser_download_url\"` -O ssg.zip
+#!/bin/bash
+wget `curl -s https://api.github.com/repos/openscap/scap-security-guide/releases/latest | jq -r ".assets[] | select(.name | test(\"(scap-security-guide-[0-9].[0-9].[0-9]*).zip\")) | .browser_download_url"` -O ssg.zip
+
